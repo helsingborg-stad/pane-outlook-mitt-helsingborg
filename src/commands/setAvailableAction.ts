0676281e8a1b27a995f7cb3d42f06a44d9e2dceb
Sepehr Noorzadeh <sepehr.noorzadeh@knowit.se>
2021-10-25 10:45:28 +0200

    fix: update title string for available time

--- src/commands/setAvailableAction.ts
+++ src/commands/setAvailableAction.ts
@@ -1,7 +1,7 @@
 /* global Office */
 
 const setAvailableAction = (event: Office.AddinCommands.Event) => {
-  Office.context.mailbox.item.subject.setAsync("Bokningsbar tid Mitt Helsingborg");
+  Office.context.mailbox.item.subject.setAsync("MH bokningsbar tid");
   // Be sure to indicate when the add-in command function is complete
   event.completed();
 };
